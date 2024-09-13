de3bbf517d3e36117a11b3dfd6bbdedf512e917c
Shorno <roy15-6141@s.diu.edu.bd>
2024-09-13 14:17:39 +0600

    email verification

---
+++ components/auth/email-verification-form.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import {useRouter, useSearchParams} from "next/navigation";
+import {useCallback, useEffect, useState} from "react";
+import {newVerificationToken} from "@/server/actions/tokens";
+import {AuthCard} from "@/components/auth/auth-card";
+import {FormSuccess} from "@/components/auth/form-success";
+import {FormError} from "@/components/auth/form-error";
+
+export const EmailVerificationForm = () => {
+    const token = useSearchParams().get("token");
+    const router = useRouter();
+    const [error, setError] = useState<string | null>("");
+    const [success, setSuccess] = useState<string | null>("");
+
+    const handleVerification = useCallback(async () => {
+        if (success || error) {
+            return;
+        }
+
+        if (!token) {
+            setError("No token found");
+            return;
+        }
+
+        const data = await newVerificationToken(token);
+        if (data.error) {
+            setError(data.error);
+        }
+        if (data.success) {
+            setSuccess(data.success);
+            // setTimeout(() => {
+            //     router.push("/auth/login");
+            // }, 3000);
+
+                router.push("/auth/login");
+
+        }
+    }, [token, success, error, router]);
+
+    useEffect(() => {
+        handleVerification();
+    }, [handleVerification]);
+
+
+    return (
+        <AuthCard cardTitle={"Verify your account"} backButtonHref={"/auth/login"} backButtonLabel={"Back to login"}>
+            <div className={"flex items-center w-full justify-center "}>
+                <p>{!success && !error ? "Verifying email..." : null}</p>
+                <FormSuccess message={success || undefined}/>
+                <FormError message={error || undefined}/>
+            </div>
+        </AuthCard>
+    );
+}
--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -1,12 +1,12 @@
 "use server"
 import {db} from "@/server";
 import {eq} from "drizzle-orm";
-import {emailTokens} from "@/server/schema";
+import {emailTokens, users} from "@/server/schema";
 
-export const getVerificationTokenByEmail = async (email: string) => {
+export const getVerificationTokenByEmail = async (token: string) => {
     try {
         return await db.query.emailTokens.findFirst({
-            where: eq(emailTokens.id, email)
+            where: eq(emailTokens.token, token)
         })
     } catch (error) {
         return null;
@@ -29,5 +29,36 @@ export const generateEmailVerificationToken = async (email: string) => {
         token,
         expires
     }).returning()
+
+}
+
+export const newVerificationToken = async (token: string) => {
+    const existingToken = await getVerificationTokenByEmail(token);
+    console.log("Existing Token", existingToken?.token);
+    if (!existingToken) return {error : "Token not found"};
+
+    const hasExpired = new Date(existingToken.expires) < new Date();
+
+    if (hasExpired) {
+        return {error: "Token has expired"}
+    }
+
+    const existingUser = await db.query.users.findFirst({
+        where: eq(users.email, existingToken.email)
+    })
+
+    if (!existingUser) {
+        return {error: "User not found"}
+    }
+
+    await db.update(users).set({
+        emailVerified: new Date(),
+        email: existingToken.email
+    })
+
+    await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
+
+
+    return {success: "Email Verified"};
 
 }
