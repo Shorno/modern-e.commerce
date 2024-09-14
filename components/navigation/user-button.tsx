418479f5b8c514f96e3b5948b58a7f08070a5356
Shorno <roy15-6141@s.diu.edu.bd>
2024-09-15 04:45:57 +0600

    theme switching

--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -1,21 +1,42 @@
 "use client"
+
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-    DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 
-import {Session} from "next-auth";
-import {signOut} from "next-auth/react";
-import {Avatar, AvatarFallback} from "@/components/ui/avatar";
-import Image from "next/image";
-import {LogOut, Moon, Settings, Sun, TruckIcon} from "lucide-react";
+import {Session} from "next-auth"
+import {signOut} from "next-auth/react"
+import {Avatar, AvatarFallback} from "@/components/ui/avatar"
+import Image from "next/image"
+import {LogOut, Moon, Settings, Sun, TruckIcon} from "lucide-react"
+import {useTheme} from "next-themes"
+import {useState, useEffect} from "react"
+import {Switch} from "@/components/ui/switch"
 
 export const UserButton = ({user}: Session) => {
-    console.log(user)
+    const {setTheme, theme} = useTheme()
+    const [checked, setChecked] = useState(false)
+
+    useEffect(() => {
+        setSwitchState()
+    }, [theme])
+
+    function setSwitchState() {
+        switch (theme) {
+            case "dark":
+                setChecked(true)
+                break
+            case "light":
+            case "system":
+                setChecked(false)
+                break
+        }
+    }
+
     if (user)
         return (
             <div className={""}>
@@ -34,7 +55,6 @@ export const UserButton = ({user}: Session) => {
                                     </AvatarFallback>
                                 )
                             }
-
                         </Avatar>
                     </DropdownMenuTrigger>
 
@@ -42,14 +62,12 @@ export const UserButton = ({user}: Session) => {
                         <div className={"mb-4 p-4 flex flex-col items-center bg-primary/15 rounded-lg gap-1"}>
                             {
                                 user.image && (
-
                                     <Image src={user.image} alt={user.name!} width={32} height={32}
                                            className={"rounded-full"}/>
                                 )
                             }
                             <p className={"font-bold text-xs"}>{user.name}</p>
                             <span className={"text-xs font-medium text-secondary-foreground"}>{user.email}</span>
-
                         </div>
                         <DropdownMenuSeparator/>
                         <DropdownMenuItem
@@ -63,19 +81,35 @@ export const UserButton = ({user}: Session) => {
                             <Settings
                                 className={"mr-2 px-1 group-hover:rotate-180 transition-all duration-300 ease-in-out"}/> Settings
                         </DropdownMenuItem>
-                        <DropdownMenuItem className={"py-2 font-medium cursor-pointer transition-all duration-500"}>
-                            <div className={"flex items-center"}>
-                                <Sun className={"px-1"}/>
-                                <Moon className={"px-1"}/>
-                                <p>
-                                    Theme <span>theme</span>
-                                </p>
-                            </div>
-                        </DropdownMenuItem>
+                        {theme && (
+                            <DropdownMenuItem className={"py-2 font-medium cursor-pointer transition-all duration-500"}>
+                                <div onClick={(e) => e.stopPropagation()}
+                                     className={"flex items-center justify-between w-full group"}>
+                                    <div className={"flex items-center"}>
+                                        <div className={"relative flex mr-2"}>
+                                            <Sun
+                                                className={"px-1 group-hover:text-yellow-600 absolute group-hover:rotate-180 dark:scale-0 dark:rotate-90 transition-all duration-500 ease-in-out"}/>
+                                            <Moon className={"px-1 group-hover:text-blue-400 dark:scale-100 scale-0"}/>
+                                        </div>
+                                        <p className={`text-xs font-bold ${theme === "dark" ? "text-blue-400" : "text-yellow-600"}`}>
+                                            {theme[0].toUpperCase() + theme.slice(1)} Mode
+                                        </p>
+                                    </div>
+                                    <Switch
+                                        className={"scale-75"}
+                                        checked={checked}
+                                        onCheckedChange={(e) => {
+                                            setChecked((prev) => !prev)
+                                            setTheme(e ? "dark" : "light")
+                                        }}
+                                    />
+                                </div>
+                            </DropdownMenuItem>
+                        )}
                         <DropdownMenuItem onClick={() => signOut()}
-                        className={"py-2 font-medium cursor-pointer focus:bg-destructive/30 transition-all duration-500 group"}
+                                          className={"py-2 font-medium cursor-pointer focus:bg-destructive/30 transition-all duration-500 group"}
                         >
-                           <LogOut size={14} className={"mr-2 group-hover:scale-90"}/> Sign Out
+                            <LogOut className={"mr-2 px-1 group-hover:scale-90"}/> Sign Out
                         </DropdownMenuItem>
                     </DropdownMenuContent>
                 </DropdownMenu>
